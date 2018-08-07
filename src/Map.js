94bbf6d727b0355b95123d58c4adb12a1334cda9
scruffy21 <yougotmebeggingyouformercy@gmail.com>
2018-08-07 11:10:01 +0200

    solved the re-rendering issue using solution from NickBrooks comment here: https://github.com/tomchentw/react-google-maps/issues/220

--- src/Map.js
+++ src/Map.js
@@ -1,29 +1,29 @@
 import React from 'react'
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
-class Map extends React.Component {
+const GoogleMapCont = withGoogleMap(props => (
+    <GoogleMap
+        defaultCenter={{ lat: 52.229676, lng: 19.012229 }}
+        defaultZoom={6}
+    >
+        {props.castles.map(castle => (
+            <Marker position={castle.location} title={castle.name} key={castle.id} onClick={() => props.castleClicked(castle.id)}/>
+        ))}
+    </GoogleMap>
+));
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return false;
-    }
+class Map extends React.Component {
 
     render() {
         console.log(this.props);
-        const GoogleMapCont = withGoogleMap(props => (
-            <GoogleMap
-                defaultCenter={{ lat: 52.229676, lng: 19.012229 }}
-                defaultZoom={6}
-            >
-                {this.props.castles.map(castle => (
-                    <Marker position={castle.location} title={castle.name} key={castle.id} onClick={() => this.props.castleClicked(castle.id)}/>
-                ))}
-            </GoogleMap>
-        ));
+
 
         return (
             <GoogleMapCont
                 containerElement={<div className='map-container' />}
                 mapElement={<div id='map' />}
+                castles={this.props.castles}
+                castleClicked={this.props.castleClicked}
             />
         )
     }
